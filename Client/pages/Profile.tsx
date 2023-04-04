bd029fdf236b11b65a3f05f6dc45cff6fbe9c70a
Rohnit Shriyan <99482544+RohnitShriyan05@users.noreply.github.com>
2023-04-04 21:37:33 +0530

    Changed Post size on Diff Screen

--- Client/pages/Profile.tsx
+++ Client/pages/Profile.tsx
@@ -4,11 +4,11 @@ import { BsChevronDoubleDown, BsPerson, BsSun } from "react-icons/bs";
 
 const Profile: FC = ()=>{
     return(
-        <div className="text-white h-max w-full bg-primaryDark flex flex-col items-center justify-center py-10vh overflow-scroll">
+        <div className="text-white h-max w-full bg-primaryDark flex flex-col items-center justify-center py-10vh">
             <BsPerson className="text-5xl"/>
             <p className="text-2xl font-bold">u/rohnitshriyan</p>
             <p className="text-sm py-2">146 Karma | 3yrs on Cheddit</p>
-            <div className="flex-1 lg:w-1/4 w-full lg:px-0 px-5vw">
+            <div className="flex-1 xl:1/4 lg:w-1/3 md:w-1/2 w-full lg:px-0 px-5vw">
                 <div className="w-full flex items-center py-2">
                     <BsSun className="mr-1"/> Sort <BsChevronDoubleDown className="ml-2 text-xs"/>
                 </div>
